0d6d3001928a8871dd0d46f8f15dc5b4f69e14fa
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-03-13 14:52:46 -0300

    feat: Retira o Menu das rotas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="menu-details">
         <div className="wrapper center">
           <BrowserRouter>
-            <Menu />
+            {/* <Menu /> */}
             <Details />
           </BrowserRouter>
         </div>
