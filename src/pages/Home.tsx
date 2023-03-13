40e0823acc1454d02b7259810964b16dcbf21ecc
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-03-13 14:59:50 -0300

    style: comenta a importação do Menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import About from "../components/About/About";
 import "./Home.scss";
 import { BrowserRouter } from "react-router-dom";
-import Menu from "../components/Menu/Menu";
+// import Menu from "../components/Menu/Menu";
 import Details from "./Details";
 
 const Home = () => {
